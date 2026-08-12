b3d3a4596469e1a79381a9fab282f7361f8828a1
Joao Victor <avesjoavictor@gmail.com>
2026-08-12 20:05:49 -0300

    Criação e configuração dos modelos de veiculo e consumo.

---
+++ mf-api-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_api_web_services_fuel_manager.Models
+{
+    [Table("consumo")]
+    public class Consumo
+    {
+        [key]
+        public int Id { get; set;  }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+        [Required]
+        public int veiculoId { get; set; }
+        public Veiculo veiculo { get; set; }
+
+    }
+
+    public enum TipoCombustivel
+    {
+        Diessel,
+        Etanol,
+        Gasolina
+    }
+}
---
+++ mf-api-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_api_web_services_fuel_manager.Models
+{
+    [Table("veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+
+        public  int  AnoModelo { get; set; }
+
+        public ICollection<consumo> consumos { get; set; }
+
+    }
+}
